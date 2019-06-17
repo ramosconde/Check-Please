4079f013508dd87f3e524bff7618b52905fa010f
Bootcamp Work <ramos_conde@yahoo.com>
2019-06-17 16:02:45 -0700

    2 digit decimals display almost working

--- JS/main2.js
+++ JS/main2.js
@@ -1,37 +1,24 @@
 
 
 d3.select("button").on("click", handleClick);
-console.log("hello22222")
-
 function handleClick() {
     d3.event.preventDefault();
     console.log("hello button");
 
 }
-console.log("this is a test")
 
-d3.select("#btn").on("click", handleClick);
-
+d3.select("#btn").on("click", calculate);
 function handleClick(){
     d3.event.preventDefault();
     console.log("hello btn");
-
 }
-
 
 function charcheck(evt) {
     var ch = String.fromCharCode(evt.which);
-    // if (!(/^\d*(\.?\d{0,2})$/.test(ch))) {
-    // if (( /^[0-9]+\.[0-9]*$/.test(ch))) {
     if (( /^[+]?[0-9]{1,3}(?:[0-9]*(?:[.,][0-9]{1})?|(?:,[0-9]{3})*(?:\.[0-9]{1,2})?|(?:\.[0-9]{3})*(?:,[0-9]{1,2})?)$/.test(ch))) {
-
         evt.preventDefault();
         console.log("charcheck does not work")
         return false;
-        // document.getElementById("demo").innerHTML = "Numbers Only";
-        // var a = "<p style=’color:red;’> Numbers Only </p>";
-        // document.write(a);
-        // displayErrMsg();
     }
     else{
         console.log("charcheck works")
@@ -56,17 +43,18 @@ function calculate() {
     var percent = parseFloat(document.getElementById("tipPercent").value);
     var guests = parseFloat(document.getElementById("numOfGuests").value);
 
-    var result1 = (total / 100) * percent;              //total tip is
-    var result2 = ((total / 100) * percent) / guests;     //tip per person
-    var result3 = (total / guests) + result1;           //tip and bill per person
-    var result4 = (total + ((total / 100) * percent));  //total bill + tip is
+    // parseFloat(Math.round(num3 * 100) / 100).toFixed(2);
+
+    var result1 = ((total / 100) * percent).toFixed(2);            //total tip is
+    var result2 = (((total / 100) * percent) / guests).toFixed(2);     //tip per person
+    var result3 = ((total / guests) + result1);          //tip and bill per person
+    var result4 = ((total + ((total / 100) * percent))).toFixed(2);  //total bill + tip is
 
     // if (!isNaN(result1)) {
-    //     // document.getElementById("total-tip").innerHTML = "The total tip is $" + result1;
-    //     // document.getElementById("tip-per-person").innerHTML = "The tip per person is $" + result2;
-    //     // document.getElementById("total-per-person").innerHTML = "The total per person is $" + result3;
-    //     // document.getElementById("total-bill").innerHTML = "The total bill is $" + result4;
-    //     console.log(result2)
+        document.getElementById("total-tip").innerHTML = "The total tip is $" + result1;
+        document.getElementById("tip-per-person").innerHTML = "The tip P/P is $" + result2;
+        document.getElementById("total-per-person").innerHTML = "Total charge + tip P/P is $" + result3;
+        document.getElementById("total-bill").innerHTML = "The total charges + total tip is $" + result4;
     // }
 
 }
@@ -76,3 +64,4 @@ var validate = function(e) {
     var t = e.value;
     e.value = (t.indexOf(".") >= 0) ? (t.substr(0, t.indexOf(".")) + t.substr(t.indexOf("."), 3)) : t;
   }
+
